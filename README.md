a38ad8a4db989cf4ceaa472539758111fdcd50ad
DavidNogu <hsd_sk8@hotmail.com>
2026-05-23 10:38:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+To make sure the app runs:
+nvm install 20
+nvm use 20
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Lab L-1 Creating a Transaction</h1>
+      <p>
+        Your transaction creation will be logged to the console (F12 to open).
+      </p>
+    </div>
+  )
+}
+
+export default App
