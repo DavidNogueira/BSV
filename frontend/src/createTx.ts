64ded1e89287bb8f6aab13bb6c6c33d373215e93
DavidNogu <hsd_sk8@hotmail.com>
2026-05-28 17:24:30 +0100

    since the locking script is derived from a pubK I see no benefit on making it a private var

--- frontend/src/createTx.ts
+++ frontend/src/createTx.ts
@@ -7,7 +7,7 @@ import {
 
 export const createTransaction = async (): Promise<void> => {
   try {
-    const lockingScript = import.meta.env.VITE_DEV_LOCKING_SCRIPT
+    const lockingScript = '2102aaa7a5a2e386840889732be8d8264d42198f116903ed9f8f2cc9763c0e9958acac0e4d7920666972737420746f6b656e0849276d204d6174744630440220187800c3732512ef3d3ccdf741966b45f4251f879ac933160837a03d1c98a420022064c4d3fb3c07b12c47aae5baef7890e996ffa680e32fb8aa678c7f06ff0d37bd6d75'
     const walletClient = new WalletClient()
     const args: CreateActionArgs = {
       description: 'Create a transaction',
