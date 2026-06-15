9e4320d1da0ef25d96737747b7503f1b076b8f3e
DavidNogu <hsd_sk8@hotmail.com>
2026-06-15 11:02:21 +0200

    the footer

--- frontend/src/Utils/footer.tsx
+++ frontend/src/Utils/footer.tsx
@@ -1,35 +1,3 @@
-import { Box, Typography, Link } from '@mui/material'
-
 export default function Footer() {
-  return (
-    <Box
-      component="footer"
-      sx={{
-        py: 3,
-        px: 2,
-        mt: 'auto',
-        textAlign: 'center',
-        borderTop: '1px solid',
-        borderColor: 'divider'
-      }}
-    >
-      <Typography variant="body2" color="text.secondary">
-        Lab L-7: ToDo List Application with PushDrop
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        Built with{' '}
-        <Link
-          href="https://docs.bsvblockchain.org/"
-          target="_blank"
-          rel="noopener"
-        >
-          BSV SDK
-        </Link>{' '}
-        and{' '}
-        <Link href="https://react.dev/" target="_blank" rel="noopener">
-          React
-        </Link>
-      </Typography>
-    </Box>
-  )
+  return <div>this is the footer</div>
 }
